4d4866a406c4141617acf963fbed309d4af602fd
Jack Cherng <jfcherng@gmail.com>
2018-03-12 01:43:35 +0800

    init

---
+++ prism.language.ass.js
@@ -0,0 +1,49 @@
+/*!
+ * ASS/SSA subtitle syntax highlight for Prism.js
+ *
+ * @author Jack Cherng <jfcherng@gmail.com>
+ * @version 1.0.0
+ * @license MIT
+ */
+Prism.languages.ass = {
+    'comment': {
+        pattern: /^([ \t]*);.*$/m,
+        inside: {
+            'space': / /
+        },
+        lookbehind: true
+    },
+    'section-name': {
+        pattern: /^([ \t]*)\[[^\]]*\](?=\s*$)/im,
+        inside: {
+            'space': / /
+        },
+        alias: 'variable'
+    },
+    'style-dialogue-comment': {
+        pattern: /^([ \t]*)(Style|Comment|Dialogue):.*$/im,
+        lookbehind: true,
+        inside: {
+            'keyword': /^(Style|Comment|Dialogue)(?=:)/im,
+            'time': {
+                pattern: /\d+:\d+:\d+\.\d+/m,
+                alias: 'string'
+            },
+            'dashed-arrow': {
+                pattern: /-+>/,
+                alias: 'punctuation'
+            },
+            'ass-tag': {
+                pattern: /\{@*\\[^}\r\n]+\}/,
+                alias: 'comment'
+            },
+            'space': / /
+        }
+    },
+    'property': {
+        pattern: /^([ \t]*)[^:]+(?=:)/im,
+        inside: {
+            'space': / /
+        }
+    }
+};
